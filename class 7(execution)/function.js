d8d3cc9a8bab5289d4c3deea55317407a7cbf27c
Pammi-Dharewa <dharewapammi@gmail.com>
2024-06-28 00:03:59 +0530

    push

---
+++ class 7(execution)/function.js
@@ -0,0 +1,32 @@
+// function print(){
+//   console.log('pammi')
+// }
+// print();
+
+// var sayhi = function(){
+//   console.log('hi')
+// }
+
+// sayhi()
+
+
+// var serveBevrage = function(quantity,drink){
+//   console.log('i want ' + quantity + ' ' + drink)
+// }
+
+// serveBevrage(3,'tea')
+
+// var a = 2
+// var b = 3
+
+// function add(a,b){
+//   return a + b
+// }
+
+// var res = add(a,b)
+// console.log(res)
+// var res2 = add(6,8)
+// console.log(res2)
+
+console.log(a)
+let a = 5
