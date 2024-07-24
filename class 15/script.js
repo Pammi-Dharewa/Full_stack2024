fae2f24052908d782ca4f85edf76a7f55113af12
Pammi-Dharewa <dharewapammi@gmail.com>
2024-07-24 21:24:32 +0530

    Push

--- class 15/script.js
+++ class 15/script.js
@@ -1,10 +1,20 @@
 
 
-async function fetchData(){
-  const endpoint = 'http://api.weatherapi.com/v1/current.json?key=22b07f36bbb74102b7a162619242207&q=Biratnagar&aqi=no'
+const searchField = document.querySelector('.searchField')
+const form = document.querySelector('.form')
+
+let target = 'Mumbai'
+form.addEventListener('submit', search)
+function search(e){
+  e.preventDefault()
+  target = searchField.value
+}
+async function fetchData(target){
+  const endpoint = `http://api.weatherapi.com/v1/current.json?key=22b07f36bbb74102b7a162619242207&q=${target}&aqi=no`
   
-  const data = await fetch(endpoint)
+  const response = await fetch(endpoint)
+  const data = await response.json()
   console.log(data)
 }
 
-fetchData();
+fetchData(target);
